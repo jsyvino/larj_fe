73075db0f5a92de50e0474ebaf1f82c986cc4036
jsyvino <jsyvino@gmail.com>
2020-06-30 15:39:16 -0500

    Updates

---
+++ precache-manifest.cf2e3648231be7ceae1901a114bc564e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c3b093ea9155392a79631920604b74",
+    "url": "/larj_fe/index.html"
+  },
+  {
+    "revision": "f203f8677945fa7e48bb",
+    "url": "/larj_fe/static/css/main.20145ef9.chunk.css"
+  },
+  {
+    "revision": "e952efb05f50c0973d3d",
+    "url": "/larj_fe/static/js/2.c4436349.chunk.js"
+  },
+  {
+    "revision": "79af7cfcc87b545295951f663bf13f4a",
+    "url": "/larj_fe/static/js/2.c4436349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f203f8677945fa7e48bb",
+    "url": "/larj_fe/static/js/main.7bc47f41.chunk.js"
+  },
+  {
+    "revision": "81f2e06eef1e1df3c167",
+    "url": "/larj_fe/static/js/runtime-main.ec24a072.js"
+  }
+]);
